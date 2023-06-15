05b454a831620a2ff0f4c87f6ff0216acbbc4f55
Armani Johnson <armanijohnson0104@gmail.com>
2023-06-15 08:12:45 -0500

    added about.js and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Intro from "./Components/Intro";
+import './Shared/globalStyles.css'
 import About from "./Components/About";
+import background from './assets/background.jpg'
 
 function App() {
   return (
+    <>
+    <img src={background} alt="background" className="Background" />
     <div className="App">
       <Intro />
       <About />
     </div>
+    </>
   );
 }
 
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import '../styles/about.css'
 
 const About = () => {
+  const one = (
+    <p>
+      Currently I am studying Computer Science
+      at <a href="https://www.hccs.edu/">Houston Community College</a>{" "} 
+      and React at <a href="https://codethedream.org/"> Code the Dream</a>. <br />
+      I am interested in any type of web development where I can 
+      use problem solving creatively.<br />
+    </p>
+  );
+  const two = (
+    <p>
+      In my free time I enjoy reading, watching art history video essays on YouTube, and
+      attending concerts and conventions.
+    </p>
+  );
+
+  const tech_stack = [
+    "Javascript",
+    "React.js",
+    "Node.js",
+    "HTML & CSS"
+  ];
+
+  const tech_items = tech_stack.map(stack => <li>{stack}</li>);
+
   return (
     <>
-    <div>
-        <h1>
+    <div className='about'>
+      <div>
+        <span className='aboutTitle'>
             About me
-        </h1>
-    </div>
+        </span>
+      </div>
     <div>
-        <p>
-            My name is Armani Johnson. Currently I am studying Computer Science
-            at Houston Community College. <br />
-            I am interested in any type of web development where I can 
-            use problem solving creatively.<br />
-
-            In my free time I enjoy reading, watching art history video essays on YouTube, and <br />
-            attending cconcerts and conventions with my family.
-        </p>
+      <div className='Description1'>
+      {[one]}
+      <ul className='Stack'>
+           <li>{tech_items}</li>
+      </ul>
+      {[two]}
+    </div>
+    </div>
     </div>
     </>
   )
