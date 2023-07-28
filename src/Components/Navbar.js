94303b1ee295cb1ba4236af294d9c0e7b4f6bf04
Armanchi <armanijohnson0104@gmail.com>
2023-07-28 10:02:52 -0500

    fixed errors

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,48 +1,41 @@
-import React from 'react'
-import '../styles/navbar.css'
-import { Sidenav } from "rsuite";
-import { EmailRounded } from '@mui/icons-material';
-import { LinkedIn } from '@mui/icons-material';
-import { GitHub } from '@mui/icons-material';
+import React from 'react';
+import '../styles/navbar.css';
+import { Sidenav } from 'rsuite';
+import { EmailRounded, LinkedIn, GitHub } from '@mui/icons-material';
 
 const Navbar = () => {
-    const links = [
-        <a href='#Intro'>home</a>,
-        <a href='#about'>about</a>,
-        <a href='#projects'>projects</a>,
-        <a href='#contact'>contact</a>
-    ];
+  const links = [
+    <a key="home" href="#Intro">home</a>,
+    <a key="about" href="#about">about</a>,
+    <a key="projects" href="#projects">projects</a>,
+    <a key="contact" href="#contact">contact</a>
+  ];
+
   return (
     <div className="nav">
-          <Sidenav
-            defaultOpenKeys={["3", "4"]}
-            appearance={"subtle"}
-          >
-            <Sidenav.Body>
-              <div className="sidebar-links">
-                {links.map((link, i) => (
-                    <div>{link}</div>
-                ))}
-              </div>
+      <Sidenav defaultOpenKeys={["3", "4"]} appearance={"subtle"}>
+        <Sidenav.Body>
+          <div className="sidebar-links">
+            {links.map((link, i) => (
+              <div key={i}>{link}</div>
+            ))}
+          </div>
           
-       
-        <div className="logos" href="/">
-           <a href="https://www.linkedin.com/in/armani-d-johnson/">
-            <LinkedIn style={{ fontSize: 21 }}></LinkedIn>
-          </a>
-          <a href="https://github.com/Armanchi">
-            <GitHub style={{ fontSize: 19 }}></GitHub>
-          </a>
-           <a href="mailto:armanijohnson0104@gmail.com">
-            <EmailRounded style={{ fontSize: 20 }}></EmailRounded>
-          </a>
-        </div>
+          <div className="logos">
+            <a href="https://www.linkedin.com/in/armani-d-johnson/">
+              <LinkedIn style={{ fontSize: 21 }} />
+            </a>
+            <a href="https://github.com/Armanchi">
+              <GitHub style={{ fontSize: 19 }} />
+            </a>
+            <a href="mailto:armanijohnson0104@gmail.com">
+              <EmailRounded style={{ fontSize: 20 }} />
+            </a>
+          </div>
         </Sidenav.Body>
-          </Sidenav>
-      </div>
-    );
-  }
-
-
+      </Sidenav>
+    </div>
+  );
+}
 
 export default Navbar;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../styles/projects.css'
-import { FiGithub } from "react-icons/fi";
-import { BsFolderFill} from "react-icons/bs";
+import '../styles/projects.css';
+import { FiGithub } from 'react-icons/fi';
+import { BsFolderFill } from 'react-icons/bs';
 
 const Projects = () => {
   const projects = {
@@ -25,39 +25,44 @@ const Projects = () => {
       link: "https://github.com/Armanchi/MyMusicApp"
     }
   };
+
   return (
-      <div id="projects">
-          <div section-header>
-              <span className="section-title">projects</span>
-              <div className="link-container">
-                  <a  href="https://github.com/Armanchi" className="hover-underline-animation">view my archive</a>
-              </div>
-          </div>
-          <div className="project-container">
+    <div id="projects">
+      <div className="section-header">
+        <span className="section-title">projects</span>
+        <div className="link-container">
+          <a href="https://github.com/Armanchi" className="hover-underline-animation">
+            view my archive
+          </a>
+        </div>
+      </div>
+      <div className="project-container">
         <ul className="projects-grid">
           {Object.keys(projects).map((key, i) => (
-              <li className="projects-card">
-                <div className="card-header">
-                  <div className="folder-icon">
-                  <BsFolderFill className="folder"/>
-                  </div>
-                  <a href={projects[key]["link"]} className="git-Link">{<FiGithub />}</a>
+            <li key={i} className="projects-card">
+              <div className="card-header">
+                <div className="folder-icon">
+                  <BsFolderFill className="folder" />
                 </div>
+                <a href={projects[key].link} className="git-Link">
+                  {<FiGithub />}
+                </a>
+              </div>
 
-                <div className="card-title">{key}</div>
-                <div className="card-desc">{projects[key]["desc"]}</div>
-                <footer>
-                <div className="card-tech">{projects[key]["techStack"]}</div>
-                </footer>
-              </li>
+              <div className="card-title">{key}</div>
+              <div className="card-desc">{projects[key].desc}</div>
+              <footer>
+                <div className="card-tech">{projects[key].techStack}</div>
+              </footer>
+            </li>
           ))}
         </ul>
         {/* <ShowMore> */}
-          {/* //TO DO: Figure out how to create working show more/less button */}
-      {/* </ShowMore>  */}
+        {/* //TO DO: Figure out how to create a working show more/less button */}
+        {/* </ShowMore> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
